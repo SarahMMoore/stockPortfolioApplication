f09ccaddd493997c9fe5904516e66fd9ee168e9b
Sarah M Moore <smmoor24@asu.edu>
2026-02-05 13:21:52 -0600

    Update PortfolioManager.java

--- src/stockPortfolio/PortfolioManager.java
+++ src/stockPortfolio/PortfolioManager.java
@@ -1,3 +1,12 @@
+/*
+ * Class Name: PortfolioManager
+ * Author: Sarah Moore
+ * Date created: 05. February 2026
+ * Purpose: Contains the main method, 
+ * maintains the Portfolio 
+ * and allows for user interaction.
+ */
+
 package stockPortfolio;
 
 public class PortfolioManager {
