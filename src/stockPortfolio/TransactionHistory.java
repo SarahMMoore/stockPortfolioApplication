7d30acbabdd6be9991211d2cdf6e2ec803850b2d
Sarah M Moore <smmoor24@asu.edu>
2026-02-26 16:58:57 -0600

    Update TransactionHistory.java

--- src/stockPortfolio/TransactionHistory.java
+++ src/stockPortfolio/TransactionHistory.java
@@ -9,56 +9,65 @@
 package stockPortfolio;
 
 public class TransactionHistory {
-    private String ticker;
-    private String transDate;
-    private String transType;
-    private double qty;
-    private double costBasis;
+	private String ticker;
+	private String transDate;
+	private String transType;
+	private double qty;
+	private double costBasis;
 
-    public TransactionHistory() {
-        this.ticker = "";
-        this.transDate = "";
-        this.transType = "";
-        this.qty = 0.0;
-        this.costBasis = 1.0;
-    }
+	public TransactionHistory() {
+		this.ticker = "";
+		this.transDate = "";
+		this.transType = "";
+		this.qty = 0.0;
+		this.costBasis = 1.0;
+	}
 
-    public TransactionHistory(String ticker, String transDate, String transType, double qty, double costBasis) {
-        this.ticker = ticker;
-        this.transDate = transDate;
-        this.transType = transType;
-        this.qty = qty;
-        this.costBasis = costBasis;
-    }
+	public TransactionHistory(String ticker, String transDate, String transType, double qty, double costBasis) {
+		this.ticker = ticker;
+		this.transDate = transDate;
+		this.transType = transType;
+		this.qty = qty;
+		this.costBasis = costBasis;
+	}
 
-    public String getTicker() { 
-    	return ticker; 
-    	}
-    public void setTicker(String ticker) { 
-    	this.ticker = ticker; 
-    	}
-    public String getTransDate() { 
-    	return transDate; 
-    	}
-    public void setTransDate(String transDate) { 
-    	this.transDate = transDate; 
-    	}
-    public String getTransType() { 
-    	return transType; 
-    	}
-    public void setTransType(String transType) { 
-    	this.transType = transType; 
-    	}
-    public double getQty() { 
-    	return qty; 
-    	}
-    public void setQty(double qty) { 
-    	this.qty = qty; 
-    	}
-    public double getCostBasis() { 
-    	return costBasis; 
-    	}
-    public void setCostBasis(double costBasis) { 
-    	this.costBasis = costBasis; 
-    	}
+	public String getTicker() {
+		return ticker;
+	}
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+	public String getTransDate() {
+		return transDate;
+	}
+
+	public void setTransDate(String transDate) {
+		this.transDate = transDate;
+	}
+
+	public String getTransType() {
+		return transType;
+	}
+
+	public void setTransType(String transType) {
+		this.transType = transType;
+	}
+
+	public double getQty() {
+		return qty;
+	}
+
+	public void setQty(double qty) {
+		this.qty = qty;
+	}
+
+	public double getCostBasis() {
+		return costBasis;
+	}
+
+	public void setCostBasis(double costBasis) {
+		this.costBasis = costBasis;
+	}
 }
